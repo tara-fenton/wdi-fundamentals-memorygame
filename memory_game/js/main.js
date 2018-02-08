eefa519164b3e69d64888d4402cdb05513f10ccf
Tara Fenton <tarafenton21@gmail.com>
2018-02-08 17:21:46 -0500

    created game logic - unit 8 assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,23 @@
 console.log("Up and running!");
+
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
